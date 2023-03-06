88202b36cb22e52f43db416bedc4e1a23fa0f012
Inpanic4 <sargyropoulos4@gmail.com>
2023-03-06 19:24:05 +0200

    done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,20 @@
 import React from "react";
+import rectangle from "../images/icon2.png";
 // import ReactDOM from "react-dom";
 
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <div className="container">
+      <h1>Fun facts about React</h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,18 @@
 import React from "react";
+import logo from "../images/icon1.png";
+import "../style.css";
 // import ReactDOM from "react-dom";
 
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav>
+      <header className="nav-items">
+        <div className="item">
+          <img src={logo} className="img" />
+          <h3 className="facts">ReactFacts</h3>
+        </div>
+        <h4 className="extra-title">React Course - Project1</h4>
+      </header>
+    </nav>
+  );
 }
